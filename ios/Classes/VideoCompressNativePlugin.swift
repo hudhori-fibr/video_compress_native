e574c5a64840d75929bc1730ad04a3d83442296c
Hadi Hudhori <hudhorisan@gmail.com>
2025-10-30 08:57:45 +0700

    medium quality

--- ios/Classes/VideoCompressNativePlugin.swift
+++ ios/Classes/VideoCompressNativePlugin.swift
@@ -257,7 +257,7 @@ private func startExport(asset: AVAsset,
 
     private func startCompressionExport(asset: AVAsset, flutterResult: @escaping FlutterResult) {
 
-        guard let session = AVAssetExportSession(asset: asset, presetName: AVAssetExportPreset1280x720) else {
+        guard let session = AVAssetExportSession(asset: asset, presetName: AVAssetExportPresetMediumQuality) else {
             flutterResult(FlutterError(code: "SESSION_FAILED", message: "Gagal membuat AVAssetExportSession", details: nil))
             return
         }
@@ -274,7 +274,7 @@ private func startExport(asset: AVAsset,
         session.shouldOptimizeForNetworkUse = true
 
     print("Starting re-encode to: \(outputURL.path)")
-    print("Using preset: \(AVAssetExportPreset1280x720)")
+    print("Using preset: \(AVAssetExportPresetMediumQuality)")
 
         self.timer?.invalidate()
         self.timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
